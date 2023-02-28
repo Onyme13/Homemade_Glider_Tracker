e0a0ebb02f1142a377282a877c21c5ac4f1271e6
Onyme-U <jocorl@hotmail.ch>
2023-02-28 13:11:05 +0100

    TEST

--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
-
-x = [1, 1]
-plt.plot(x)
-plt.show()
+import numpy as np
+# set up orthographic map projection with
+# perspective of satellite looking down at 50N, 100W.
+# use low resolution coastlines.
+map = Basemap(projection='ortho',lat_0=45,lon_0=-100,resolution='l')

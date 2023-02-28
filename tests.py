0a3dca199acf0d2257d0c23e46dac49460196728
Joël Cori <jocorl@hotmail.ch>
2023-02-28 08:34:38 +0100

    ADD changes on tests.py to test cartopy on Linux

--- tests.py
+++ tests.py
@@ -1,7 +1,11 @@
+import urllib.request
+import json
 import matplotlib.pyplot as plt
 from matplotlib import animation
 import cartopy.crs as ccrs
+from cartopy.io.img_tiles import OSM
 
+#DEFINE FIGURE
 fig, ax = plt.subplots()
 
 #SET AXES FOR PLOTTING AREA
@@ -10,9 +14,9 @@ ax.set_ylim(40.6051,40.6825)
 ax.set_xlim(-73.8288,-73.7258)
 
 #ADD OSM BASEMAP
-#ax.add_image(osm_tiles,13) #Zoom level 13
+osm_tiles=OSM()
+ax.add_image(osm_tiles,13) #Zoom level 13
 
 #PLOT JFK INTL AIRPORT
 ax.text(-73.778889,40.639722,'JFK Intl',horizontalalignment='right',size='large')
 ax.plot([-73.778889],[40.639722],'bo') #Plot a point in blue color
-plt.show()

403386ec99c54135ae1f986fb9307c3120ec7318
Joël Cori <jocorl@hotmail.ch>
2023-06-19 20:55:42 +0200

    PERF

--- main.py
+++ main.py
@@ -1,134 +1,79 @@
 import serial
 from datetime import datetime
+import itertools
+import time
 
+arduinoSerial = serial.Serial('COM4', 9600)
 
-
-arduinoSerial = serial.Serial('COM4',9600)
-#arduinoSerial = serial.Serial('COM3',9600)
-#arduinoSerial = serial.Serial('COM4',115200)
-
-
-
-SEALEVELPRESSURE = 1013.25 
+SEALEVELPRESSURE = 1013.25
 METERSOFAIRHETCOPASCAL = 0.12677457000000025
 HETCOPASCALMETERSOFAIR = 7.8880172892718
-FEETSPERMETER = 3.28084
 
-returnDict = {
-        "time": 0,
-        "localQNH": 0,
-        "lat" : 0,
-        "long" : 0,
-        "speed": 0,
-        "altGPS": 0,
-        "sat":0,
-        "vert":0,
-        "alt": 0
-    }    
-
-verticalSpeed = 0
-altitudeBucket = [] #Bucket for the altitude data of the barometric sensor
-oldAlt = 0 #Old altitude data for measuring vertical speed
-altGPSArray = [] #Array of the altitude data from the GPS
-localQNH = 0 #local QNH difference with the sea level pressure
+previous_altitude = 0
+previous_time = 0
+altitudeBucket = []
+altGPSArray = []
+localQNH = 0
 heightCalibrated = 0
-calibrated = False #boolean for the calibration of the local QNH
-altBucket = []
+calibrated = False  
 
-
-def adapt_local_pressure(altGPS,alt):
-    global calibrated
+def adapt_local_pressure(altGPS, alt):
     global localQNH
     global heightCalibrated
+    global calibrated
 
-    altitude_proximity_counter=0
-    
+    altitude_proximity_counter = 0
+
     altGPSArray.append(altGPS)
-    
+
     if len(altGPSArray) > 20:
         altGPSArray.pop(0)
-        for x in range (len(altGPSArray)):
-            for y in range (len(altGPSArray)):
-                if altGPSArray[x] < altGPSArray[y] + 3.5 and altGPSArray[x] > altGPSArray[y] - 3.5 and altGPSArray[x]!=0.0:
-                    altitude_proximity_counter+=1
-                else:
-                    break
+        if all(abs(x - y) < 3.5 for x, y in itertools.combinations(altGPSArray, 2) if x != 0.0):
+            localQNH = SEALEVELPRESSURE + ((sum(altGPSArray) / len(altGPSArray)) - alt) * METERSOFAIRHETCOPASCAL
+            heightCalibrated = round((localQNH - SEALEVELPRESSURE) * HETCOPASCALMETERSOFAIR, 1)
+            calibrated = True  
 
-    if altitude_proximity_counter == 400:
-        localQNH = SEALEVELPRESSURE + ((sum(altGPSArray)/20)-alt) * METERSOFAIRHETCOPASCAL
-        heightCalibrated = round((localQNH - SEALEVELPRESSURE) * HETCOPASCALMETERSOFAIR,1)
-        calibrated=True
-
-#test of new function that uses threading, every second call this function.
-def vert_speed_test(alt):
-    global altBucket
-
-    while len(altBucket) < 2:
-        altBucket.append(alt)
-
-    
-    verticalSpeed = round(altBucket[1]-altBucket[0],1) 
-
-    altBucket.pop(altBucket[0])
-
-    if verticalSpeed == -0.0:
-        verticalSpeed = 0.0
-
-    return verticalSpeed
-
-#calculates vertical speed
 def vertical_speed(alt):
-    global verticalSpeed
-    global newAlt
-    global oldAlt
+    global previous_altitude, previous_time
+    current_time = time.time()
+    delta_time = current_time - previous_time
+    altitude_difference = alt - previous_altitude
+    vertical_speed = altitude_difference / delta_time
+    
+    # Update previous values for the next iteration
+    previous_altitude = alt
+    previous_time = current_time
+    
+    vertical_speed = round(vertical_speed, 1)
+    return vertical_speed
 
-    newAlt = alt
-    altitudeBucket.append(alt)
-    if len(altitudeBucket) > 4: #data is received every 0.25 every 4*0.25=1sec calculate vertical speed in m/s  
-        altitudeBucket.pop(0)
-    newAlt = sum(altitudeBucket)/4
-
-    verticalSpeed = round(newAlt-oldAlt,1) 
-
-    if verticalSpeed == -0.0:
-        verticalSpeed = 0.0
-
-    oldAlt=newAlt
-
-    return verticalSpeed  
-
-
-#main function that processes data
 def data():
-    while(arduinoSerial.inWaiting()==0):
+    #time.sleep(1)
+    while arduinoSerial.inWaiting() == 0:
         pass
     dataString = arduinoSerial.readline().decode('utf-8')
-    dataArray = dataString.replace('\r\n','').split(',')
+    dataArray = dataString.replace('\r\n', '').split(',')
 
-    localtime = datetime.now()
-    localtime = localtime.strftime("%H:%M:%S.%f")[:-3]
-    returnDict.update({
-        'time':localtime
-    })
-    
-    #If lenght of dataArray is only one, this means the only output is barometric height
+    localtime = datetime.now().strftime("%H:%M:%S.%f")[:-3]
+    returnDict = {
+        "time": localtime,
+        "localQNH": 0,
+        "lat": 0,
+        "long": 0,
+        "speed": 0,
+        "altGPS": 0,
+        "sat": 0,
+        "vert": 0,
+        "alt": 0
+    }
+
     if len(dataArray) == 1:
-
         altitude = float(dataArray[0])
         vert_speed = vertical_speed(altitude)
-        #altitude = float(dataArray[0])
-        if calibrated: #If QNH is calibrated, adjust the height.
+        if calibrated:
             altitude += heightCalibrated
-            returnDict.update({'localQNH':localQNH,})
-
-        returnDict.update({
-            "vert": vert_speed,
-            "alt":altitude
-            })
-        
-    #dataArray[latitude, longitude, speed, altitude of GPS, altitude barometric] (metric system)
-    if len(dataArray) >= 5:
-
+        returnDict.update({"vert": vert_speed, "alt": altitude})
+    elif len(dataArray) >= 5:
         latitude = float(dataArray[0]) 
         longitude = float(dataArray[1])
         speed = float(dataArray[2])
@@ -137,28 +82,26 @@ def data():
         altitude = float(dataArray[-1])
 
         vert_speed = vertical_speed(altitude)
-        
-        if calibrated: #If QNH is calibrated, adjust the height.
+
+        if not calibrated:
+            adapt_local_pressure(altitudeGPS, altitude)
+
+        if calibrated:
             altitude += heightCalibrated
 
-        #If QNH is not calibrated, calibrate the local QNH with GPS data
-        if not calibrated:
-            adapt_local_pressure(altitudeGPS,altitude)
-        
         returnDict.update({
-            "lat" : latitude,
-            "long" : longitude,
+            "lat": latitude,
+            "long": longitude,
             "speed": speed,
             "altGPS": altitudeGPS,
-            "sat":satellites,
+            "sat": satellites,
             "vert": vert_speed,
             "alt": altitude
         })
-    #print serial data from Arduino
-    #print(dataArray)
+
     print(returnDict)
     return returnDict
 
-#testing output data
+
 #while True:
 #    data()

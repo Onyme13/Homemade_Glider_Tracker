d15c364355732e70159a0ead87bedce6a1f70508
Onyme <77994657+Onyme13@users.noreply.github.com>
2023-07-12 18:11:18 +0200

    Update startup_launch.sh

--- startup_launch.sh
+++ startup_launch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+#!/usr/bin/env python3
+
 sleep 10
-sudo python3 server.py
+sudo python3 /home/onzme/Research-Project/server.py
 sleep 5
-sudo python3 GUI_tkinter.py
+sudo python3 /home/onzme/Research-Project/GUI_tkinter.py
